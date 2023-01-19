62246dfe97a9ac7e619986baa550012cd6b2b791
nesa402610 <argyleferris@gmail.com>
2023-01-19 15:14:12 +0300

    Проверяем наличие данных в ЛС

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-
 import {Outlet} from "react-router-dom";
 import {Header} from "./components/header";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {signIn} from "./store/slices/userSlice";
 
 export function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const ls = localStorage.getItem('id')
+    if (ls) {
+      dispatch(signIn(ls))
+    }
+  }, [dispatch]);
   return (
     <>
       <Header/>
