20b5bf3dd6a066458c95506457e2814b4eeb4956
nesa402610 <argyleferris@gmail.com>
2023-01-19 15:15:58 +0300

    Оборачиваем в провайдер редакса

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {RouterProvider} from "react-router-dom";
 import {router} from "./pages/router";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router}/>
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
 );
 
