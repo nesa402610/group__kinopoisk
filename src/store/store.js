e9dc91425ba919031d90d33723de1e1130315795
nesa402610 <argyleferris@gmail.com>
2023-01-19 15:14:55 +0300

    Создан полноценный стор редакса

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import userSlice from "./slices/userSlice";
+import filmsSlice from "./slices/filmsSlice";
 
+const rootReducer = combineReducers({
+  user: userSlice,
+  films: filmsSlice
+});
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: rootReducer,
+});
