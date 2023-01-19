a1758fc943d63336d98ddf03811ea21aa2e7d5e8
nesa402610 <argyleferris@gmail.com>
2023-01-19 22:47:08 +0300

    Добавлены роуты и базовый вид

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,24 @@
 import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 export function Header() {
+  const {isAuth, id} = useSelector(state => state.user)
   return (
     <header className={'bg-neutral-800'}>
       <div className={'flex justify-between  mx-8 py-4'}>
-       <div className={'gap-4 flex'}>
-         <Link to={'/'}>Главная</Link>
-         <Link to={'/'}>Поиск</Link>
-         <Link to={'/'}>Фильмы</Link>
-         <Link to={'/'}>О проекте</Link>
-       </div>
         <div className={'gap-4 flex'}>
-         <Link to={'/signup'}>Регистрация</Link>
-         <Link to={'/signin'}>Вход</Link>
+          <Link to={'/'}>Главная</Link>
+          <Link to={'/films'}>Фильмы</Link>
+          <Link to={'/about'}>О проекте</Link>
         </div>
+        {!isAuth ?
+          <div className={'gap-4 flex'}>
+          <Link to={'/signup'}>Регистрация</Link>
+          <Link to={'/signin'}>Вход</Link>
+        </div>
+          :
+          <div>Профиль {id}</div>
+        }
       </div>
     </header>
   );
