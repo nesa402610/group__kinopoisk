5d113331979e9254b748795b965d649860b7e06e
nesa402610 <argyleferris@gmail.com>
2023-01-19 17:05:45 +0300

    Создан класс для запросов и взаимодействия с API

---
+++ src/API/kinopoisk.js
@@ -0,0 +1,14 @@
+const token = 'QCZ222H-G48M6H1-GF5EB9G-GRDGM91'
+class Kinopoisk {
+  constructor(data) {
+    this.token = data.token
+    this.user_id = data.user_id
+  }
+
+}
+
+
+export const api = new Kinopoisk({
+  user_id: localStorage.getItem('id'),
+  token: token
+})
