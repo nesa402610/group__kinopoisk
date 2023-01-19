2f14dc03a0783a33b5f5ecb37fc8da2996eb5398
nesa402610 <argyleferris@gmail.com>
2023-01-19 14:03:17 +0300

    Ссылки в хедере

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,18 @@ import {Link} from "react-router-dom";
 
 export function Header() {
   return (
-    <header>
-      <div>
-        <Link to={'/'}>Главная</Link>
-        <Link to={'/'}>Поиск</Link>
-        <Link to={'/'}>Фильмы</Link>
-        <Link to={'/'}>О проекте</Link>
+    <header className={'bg-neutral-800'}>
+      <div className={'flex justify-between  mx-8 py-4'}>
+       <div className={'gap-4 flex'}>
+         <Link to={'/'}>Главная</Link>
+         <Link to={'/'}>Поиск</Link>
+         <Link to={'/'}>Фильмы</Link>
+         <Link to={'/'}>О проекте</Link>
+       </div>
+        <div className={'gap-4 flex'}>
+         <Link to={'/signup'}>Регистрация</Link>
+         <Link to={'/signin'}>Вход</Link>
+        </div>
       </div>
     </header>
   );
