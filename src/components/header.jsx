268f5aadb60804a98c79aa31bfd84073c507e777
nesa402610 <argyleferris@gmail.com>
2023-01-20 10:54:01 +0300

    Добавлен поиск

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,35 @@
 import {Link} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {setSearch} from "../store/slices/filmsSlice";
 
 export function Header() {
-  const {isAuth, id} = useSelector(state => state.user)
+  const {isAuth, id} = useSelector(state => state.user);
+  const {search} = useSelector(state => state.films);
+  const dispatch = useDispatch();
   return (
     <header className={'bg-neutral-800'}>
-      <div className={'flex justify-between  mx-8 py-4'}>
-        <div className={'gap-4 flex'}>
+      <div className={'flex justify-between gap-8 mx-8 py-4'}>
+        <div className={'gap-4 flex items-center'}>
           <Link to={'/'}>Главная</Link>
           <Link to={'/films'}>Фильмы</Link>
           <Link to={'/about'}>О проекте</Link>
         </div>
-        {!isAuth ?
-          <div className={'gap-4 flex'}>
-          <Link to={'/signup'}>Регистрация</Link>
-          <Link to={'/signin'}>Вход</Link>
+        <div className={'flex-1'}>
+          <input type="text"
+                 className={'w-full py-2 px-4 bg-neutral-700 outline-0'}
+                 value={search}
+                 onChange={e => dispatch(setSearch(e.target.value))}/>
         </div>
-          :
-          <div>Профиль {id}</div>
-        }
+        <div className={'gap-4 flex items-center'}>
+          {!isAuth ?
+            <>
+              <Link to={'/signup'}>Регистрация</Link>
+              <Link to={'/signin'}>Вход</Link>
+            </>
+            :
+            <div>Профиль {id}</div>
+          }
+        </div>
       </div>
     </header>
   );
