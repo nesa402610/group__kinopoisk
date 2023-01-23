e2e0d5bb0d174389b1a968e4608a95b3a10e93a2
nesa402610 <argyleferris@gmail.com>
2023-01-23 16:02:04 +0300

    Примерный вид страницы набросал

--- src/pages/about/aboutPage.jsx
+++ src/pages/about/aboutPage.jsx
@@ -2,11 +2,13 @@ export function AboutPage() {
   const authors = [
     {
       name: 'Феррис Аргайл',
-      info: 'инфа',
+      info: 'А тут я',
+      checkList: ['Страница регистрации и авторизации', 'Страница "О проекте"', 'Создал базу приложения'],
     },
     {
       name: 'Владимир Матвеев',
-      info: 'инфа',
+      info: 'Тут крч ты пропишешь, что делал',
+      checkList: ['Главная страница', 'Страница фильмов', 'Аренда хоста и настройка сервера'], // а это по пунктам, что делал
     },
   ]
   return (
@@ -15,13 +17,35 @@ export function AboutPage() {
         <h1 className="text-2xl font-bold text-center my-4">Над проектом работали</h1>
         <div className="flex gap-4">
           {authors.map((author) => (
-            <div className="bg-neutral-700 p-4 flex-1 rounded-lg">
+            <div className="flex flex-col gap-2 bg-neutral-700 p-4 flex-1 rounded-lg">
               <h2 className="font-bold">{author.name}</h2>
               <div>
                 {author.info}
               </div>
+              <div className="bg-neutral-800 p-2 rounded-lg">
+                <ul className="list-disc list-inside">
+                  {author.checkList.map((item) => (
+                    <li>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           ))}
+        </div>
+      </div>
+      <div>
+        <h1 className="text-2xl font-bold text-center my-4">Информация о проекте</h1>
+        <div className="bg-neutral-700 p-4 rounded-lg">
+          <p>Дипломный проект, разрабатываемый в команде. </p>
+          <ul>
+            <li>ReactJS</li>
+            <li>Axios</li>
+            <li>TailwindCSS</li>
+            <li>ReactJS</li>
+            <li>ReactJS</li>
+          </ul>
         </div>
       </div>
     </div>
