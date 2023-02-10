32b0f718d4a25dbc8941bad880fbbb2a86dabbca
andrey-glazkov-frontend <dr-greenfake@yandex.ru>
2023-02-10 20:19:00 +0300

    adde newest functioonal of dropdown

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -1,37 +1,50 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useSearchParams } from 'react-router-dom'
 import { setSearch } from '../../store/slices/filmsSlice'
+import { useDebounce } from '../coustomHooks/useDebounse'
 
 import style from './Dropdown.module.css'
 
 export default function Dropdown() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
   const { search } = useSelector((state) => state.films)
   const dispatch = useDispatch()
   const container = useRef()
   const [dropdownStateWithInput, setdropdownStateWithInput] = useState({ open: false })
   const [dropdownStateWithNoInput, setdropdownStateWithNoInput] = useState({ open: false })
+  const [input, setInput] = useState(() => searchParams.get('q') ?? '')
+  const debounceValue = useDebounce(input, 350)
+
+  useEffect(() => {
+    setSearchParams({ q: input })
+  }, [input])
+
+  useEffect(() => {
+    setSearchParams({
+      ...Object.fromEntries(searchParams.entries()),
+      search: input,
+    })
+  }, [input])
 
   const handleDropdownInput = (e) => {
     dispatch(setSearch(e.target.value))
     setdropdownStateWithInput({ open: true })
+    setInput(e.target.value)
   }
+
   const handleDropdownClick = (e) => {
-    if (e.target === 'mousedown') {
+    if (e.target.value === '') {
       setdropdownStateWithNoInput({ open: true })
+    // } else if (dropdownStateWithNoInput.open === 'true' && e.target.value !== '') {
+    //   handleDropdownInput(e)
+    } else {
+      handleDropdownInput(e)
     }
   }
-
-  //   const searchHandler = (e) => {
-  //     if (search === '') {
-  //       handleDropdownClick(e)
-  //     } else {
-  //       handleDropdownInput(e)
-  //     }
-  //   }
-
   const handleClickOutside = (e) => {
     if (container.current && !container.current.contains(e.target)) {
-    // if (search === '') {
       setdropdownStateWithInput({ open: false })
       setdropdownStateWithNoInput({ open: false })
     }
@@ -42,17 +55,27 @@ export default function Dropdown() {
     return () => document.removeEventListener('mousedown', handleClickOutside)
   }, [search])
 
+  useEffect(() => {
+    dispatch(setSearch((debounceValue)))
+  }, [debounceValue])
+
+  useEffect(() => {
+    setSearchParams({ q: input })
+  }, [input])
+
   return (
-    <div className={`${style.container} flex-1`} ref={container}>
+    <div className=" flex-1" ref={container}>
 
       <input
         type="text"
         className="w-full text-neutral-300 focus-within:text-white rounded-full focus-visible:outline outline-2 outline-neutral-500 py-2 px-4 bg-neutral-700"
         value={search}
         onChange={(e) => handleDropdownInput(e)}
+        onClick={(e) => handleDropdownClick(e)}
       />
       {dropdownStateWithInput.open && (
-      <div className={style.dropdown}>
+      //   <div className="absolute z-40 w-100 bg-white top-5% left-30 right-30 w-3/6">
+      <div className={style.container}>
         <ul>
           <li>Item 1</li>
           <li>Item 2</li>
@@ -63,7 +86,7 @@ export default function Dropdown() {
       )}
 
       {dropdownStateWithNoInput.open && (
-      <div className={style.dropdown}>
+      <div className={style.container}>
         <ul>
           <li>12</li>
           <li>23</li>
