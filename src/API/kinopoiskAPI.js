27c15d4fb446b20d2a96ae661c20a860f84f534a
nesa402610 <argyleferris@gmail.com>
2023-02-12 13:36:34 +0300

    Новый эндпоинт для фильма по ID

--- src/API/kinopoiskAPI.js
+++ src/API/kinopoiskAPI.js
@@ -1,8 +1,9 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Kinopoisk } from './kinopoisk'
 
 export const kinopoiskApi = createApi({
   reducerPath: 'kinopoiskApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/' }),
   endpoints: (builder) => ({
     getTop250: (builder.query({
       queryFn: async ({ page, type }) => {
@@ -13,7 +14,16 @@ export const kinopoiskApi = createApi({
           return { error: error.message }
         }
       },
-
     })),
+    getFilm: builder.query({
+      query: (id) => ({
+        url: `${id}`,
+        headers: {
+          'X-API-KEY': 'a2810fed-e498-4fe2-a69a-b14b641fa617',
+        },
+      }),
+    }),
   }),
 })
+
+export const { useGetTop250Query, useGetFilmQuery } = kinopoiskApi
